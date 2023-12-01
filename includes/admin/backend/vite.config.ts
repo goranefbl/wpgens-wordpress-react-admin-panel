e2a902df3e758f8fc7a8ebe6cce012eef0d7fd27
Goran <goranefbl@gmail.com>
2023-12-01 11:19:32 +0100

    config explained

--- includes/admin/backend/vite.config.ts
+++ includes/admin/backend/vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,16 +8,16 @@ export default defineConfig({
 	build: {
 		rollupOptions: {
 			output: {
-				entryFileNames: `[name].js`,
-				chunkFileNames: `[name].js`,
-				assetFileNames: `[name].[ext]`,
-				dir: `../assets/js`,
-				manualChunks: undefined,
-				format: "iife", // We need iife or else globals will clash with WordPress global variables.
+				entryFileNames: `[name].js`, // No need for hash, you have plugin version to deal with caching.
+				chunkFileNames: `[name].js`, // No need for hash, you have plugin version to deal with caching.
+				assetFileNames: `[name].[ext]`, // No need for hash, you have plugin version to deal with caching.
+				dir: `../assets/js`, // This is assets folder one directory below backend folder.
+				manualChunks: undefined, // We want single JS file. By default its split into index and vendor.
+				format: 'iife', // We need iife or else globals will clash with WordPress global variables!
 			},
 		},
 	},
 	server: {
-		port: 5178,
+		port: 5178, // Port running our app, its hardcoded under the class-wp-react-admin-panel-assets.php
 	},
 });
